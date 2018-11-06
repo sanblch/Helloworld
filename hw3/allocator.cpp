ebbb6e33968c8638198345a08b19750bfdf1231a
Alexander Krutikov <a.krutikov@innopolis.ru>
2018-11-06 16:30:10 +0300

    done hw3

---
+++ hw3/allocator.cpp
@@ -0,0 +1,34 @@
+#include "logging_allocator.h"
+#include "mycontainer.h"
+#include "reserving_allocator.h"
+#include <iostream>
+#include <map>
+
+int main(int, char *[]) {
+  auto m1 = std::map<int, int>{};
+  for(size_t i = 0; i < 10; ++i) {
+    m1[i] = (i == 0 ? 1 : m1[i - 1] * i);
+  }
+
+  auto m2 = std::map<const int, int, std::less<int>, reserving_allocator<std::pair<const int, int>, 10>>{};
+  for(size_t i = 0; i < 10; ++i) {
+    m2[i] = (i == 0 ? 1 : m2[i - 1] * i);
+    std::cout << i << " " << m2[i] << std::endl;
+  }
+  std::cout << std::endl;
+
+  auto l1 = MyContainer<int>{};
+  for(int i = 0; i < 10; ++i) {
+    l1.push_back(i);
+  }
+
+  auto l2 = MyContainer<int, reserving_allocator<MyItem<int>, 10>>{};
+  for(int i = 0; i < 10; ++i) {
+    l2.push_back(i);
+  }
+  for(auto i : l2)
+    std::cout << i << std::endl;
+  std::cout << std::endl;
+
+  return 0;
+}
---
+++ hw3/reserving_allocator.h
@@ -0,0 +1,64 @@
+#include <iostream>
+
+template<typename T, std::size_t N>
+struct reserving_allocator {
+  using value_type = T;
+
+  using pointer = T*;
+  using const_pointer = const T*;
+  using reference = T&;
+  using const_reference = const T&;
+
+  reserving_allocator() {
+    init = alloc(N);
+    curr = init;
+  }
+
+  ~reserving_allocator() {
+    deallocate(init, N);
+  }
+
+  template<typename U>
+  struct rebind {
+    using other = reserving_allocator<U, N>;
+  };
+
+  T* allocate(std::size_t n) {
+    T* res;
+    if(n <= static_cast<std::size_t>((init + N) - curr)) {
+      res = curr;
+      curr += n;
+    } else {
+      res = alloc(n);
+    }
+    return res;
+  }
+
+  void deallocate(T *p, std::size_t n) {
+    if(p >= init + N) {
+      std::free(p);
+    } else if(p == init && n == N) {
+      std::free(p);
+    }
+  }
+
+  template<typename U, typename ...Args>
+  void construct(U *p, Args &&...args) {
+    new(p) U(std::forward<Args>(args)...);
+  }
+
+  void destroy(T *p) {
+    p->~T();
+  }
+
+private:
+  T* alloc(std::size_t n) {
+    auto p = std::malloc(n * sizeof(T));
+    if (!p)
+      throw std::bad_alloc();
+    return reinterpret_cast<T *>(p);
+  }
+
+  T* init;
+  T* curr;
+};
